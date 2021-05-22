16a10ae3350f96e6e655a8040a6a2e18a54b71ba
Animesh Lal <Animesh.Lal@go-mmt.com>
2021-05-22 12:41:29 +0530

    mapper and hibernate issue fix

--- src/main/java/BankingApplication/BankingApplication.java
+++ src/main/java/BankingApplication/BankingApplication.java
@@ -6,10 +6,15 @@ import BankingApplication.resources.BankingApplicationResource;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.hibernate.ScanningHibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class BankingApplication extends Application<BankingApplicationConfiguration> {
+
+    protected HibernateBundle<BankingApplicationConfiguration> hibernate;
 
     public static void main(final String[] args) throws Exception {
         new BankingApplication().run(args);
@@ -23,12 +28,19 @@ public class BankingApplication extends Application<BankingApplicationConfigurat
     @Override
     public void initialize(final Bootstrap<BankingApplicationConfiguration> bootstrap) {
         super.initialize(bootstrap);
+        hibernate = new ScanningHibernateBundle<BankingApplicationConfiguration>("BankingApplication.model") {
+            @Override
+            public DataSourceFactory getDataSourceFactory(BankingApplicationConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        };
+        bootstrap.addBundle(hibernate);
     }
 
     @Override
     public void run(final BankingApplicationConfiguration configuration,
                     final Environment environment) {
-        Injector injector = Guice.createInjector(new BankingApplicationModule(configuration.getPropertyFileName()));
+        Injector injector = Guice.createInjector(new BankingApplicationModule(configuration.getPropertyFileName(), hibernate.getSessionFactory()));
         environment.jersey().register(injector.getInstance(BankingApplicationResource.class));
         environment.healthChecks().register("BankingApplicationHealthCheck", new BankingApplicationHealthCheck());
     }
--- src/main/java/BankingApplication/dao/TransactionDAO.java
+++ src/main/java/BankingApplication/dao/TransactionDAO.java
@@ -1,15 +1,19 @@
 package BankingApplication.dao;
 
 import BankingApplication.model.TransactionModel;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import io.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Singleton
 public class TransactionDAO extends AbstractDAO<TransactionModel> {
 
     private final Logger logger = LoggerFactory.getLogger(TransactionDAO.class);
 
+    @Inject
     public TransactionDAO (SessionFactory sessionFactory) {
         super(sessionFactory);
     }
